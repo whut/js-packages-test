c19d539b78758142e0eb5e63d5b5a54f1a6846ac
Rafał Kłys <rafalklys@wp.pl>
2023-03-29 16:09:20 +0200

    Simple JS monorepo with babel
    Go to definition and debugger breakpoits work in VSCode

---
+++ packages/app/src/index.js
@@ -0,0 +1,12 @@
+import { hello } from 'lib-a'
+
+const delay = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+
+for (;;) {
+  console.log(hello())
+  // eslint-disable-next-line no-await-in-loop, no-magic-numbers
+  await delay(1000)
+}
