20f294b48160fa4d6ce02fee763834b080ad8909
Rafał Kłys <rklys@sii.pl>
2023-04-21 22:04:29 +0200

    Init

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import { Dupa2 } from "./mymodule"
+
+type Color = "red" | 'green' | 'white'
+
+class Dupa{
+
+}
+
+const a: number = 6;
+ 
+// switch (a) {
+//   case 0:
+//     console.log("even");
+//   case 1:
+//     console.log("odd");
+//     break;
+// }
+// class Album {
+//     setup() {
+//       // Default behavior
+//     }
+//   }
+   
+//   class MLAlbum extends Album {
+//     setup() {
+//       // Override to get info from algorithm
+//     }
+//   }
+
+//   function lookupHeadphonesManufacturer(color: "blue" | "black"): string {
+//       if (color === "blue") {
+//         return "beats";
+//       } else {
+//         "bose";
+//       }
+//     }
+
+// test
+new Dupa2()
+
+function maluj(color: Color){
+    console.log(`Pomalowalem na ${color}`)
+}
+
+maluj("red")
+maluj("red")
+
+enum Color2 {
+    Red,
+    Green,
+    White
+}
+
+function maluj2(color: Color2){
+    console.log(`Pomalowalem na ${color}`)
+}
+
+maluj2(Color2.Green)
+maluj2(Color2.Red)
+maluj2(1)
+
+// Console.WriteLine("sadsa");
+
+// int wynik = 1 + 2
+
+// if(costam == 1){
+//     wynik = 1000
+// } 
+// else {
+//     wynik = -1000
+// }
+
+// wynik = costam ? 1000 : -1000
+
+// ||
